a9837e85fe5338c1d6f2813c3b155a2d1899a2f2
amran <amranzakaria2026@gmail.com>
2026-03-26 12:08:02 +0800

    update .env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-
-// Replace these with your actual Supabase project URL and anon key
-const SUPABASE_URL = 'https://bwoilbzuwquopjprgnhw.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_st34FTUKq-jsKG5IyYXSoQ_G2TRvgKT';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
